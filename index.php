923d947a019207135f3e1c7df80cc0494f2b955d
AugustoM94 <email@github.com>
2023-12-04 16:16:11 +0100

    step1 completed

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
-
 <?php
 
 class Movie 
 {
-    private  $id;
-    private  $title;
-    private  $overview;
-    private  $vote_average;
-    private  $poster_path;
-    private  $original_language;
+    private $id;
+    private $title;
+    private $overview;
+    private $vote_average;
+    private $poster_path;
+    private $original_language;
 
     public function __construct($id, $title, $overview, $vote, $poster_path, $original_language) 
     {
@@ -20,6 +19,19 @@ class Movie
         $this->original_language = $original_language;
     }
 
+    public function print()
+    {
+        echo "<h2>{$this->title}</h2>";
+        echo "<p>ID: {$this->id}</p>";
+        echo "<p>Overview: {$this->overview}</p>";
+        echo "<p>Vote Average: {$this->vote_average}</p>";
+        echo "<img src='{$this->poster_path}' alt='logo'>";
+        echo "";
+        echo "<p>Original Language: {$this->original_language}</p>";
+        echo "<hr>";
+
+        
+    }
 }
 
 $movieString = file_get_contents(__DIR__ . '/model/movie_db.json');
@@ -29,9 +41,8 @@ $movies = [];
 foreach ($movieList as $movie) {
     $movies[] = new Movie($movie['id'], $movie['title'], $movie['overview'], $movie['vote_average'], $movie['poster_path'], $movie['original_language']);
 }
-var_dump($movies);
-?>
 
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -40,11 +51,17 @@ var_dump($movies);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />    <link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
     <title>PHP - Oop</title>
 </head>
 <body>
-    
+    <main>
+        <?php
+        foreach ($movies as $movie) {
+            $movie->print();
+        }
+        ?>
+    </main>
 </body>
 </html>
